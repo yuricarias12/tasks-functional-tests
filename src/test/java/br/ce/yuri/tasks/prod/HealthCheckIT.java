2f62fcc6362e0a7f8eb192aa129dacbe6b132461
Carias <yuristricken7@gmail.com>
2025-02-10 12:45:21 -0300

    Health Check

---
+++ src/test/java/br/ce/yuri/tasks/prod/HealthCheckIT.java
@@ -0,0 +1,30 @@
+package br.ce.yuri.tasks.prod;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class HealthCheckIT {
+	@Test
+	public void healthCheck() throws MalformedURLException {
+		DesiredCapabilities cap = DesiredCapabilities.chrome();
+		WebDriver driver = new RemoteWebDriver(new URL(" http://10.0.0.106:4444/wd/hub"), cap);
+		try {
+			driver.navigate().to("http://10.0.0.106:9999/tasks");
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			String version = driver.findElement(By.id("version")).getText();
+			Assert.assertTrue(version.startsWith("build"));
+		} finally {
+			driver.quit();
+		}
+		
+	}
+
+}
