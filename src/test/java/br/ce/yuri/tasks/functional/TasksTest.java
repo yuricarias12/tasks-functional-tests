9180d7c657ccdb98fad80cc0bb16a53380fd5156
Carias <yuristricken7@gmail.com>
2025-02-11 13:49:13 -0300

    teste de remoção

--- src/test/java/br/ce/yuri/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/yuri/tasks/functional/TasksTest.java
@@ -124,5 +124,33 @@ public class TasksTest {
 		}
 
 	}
+	
+	@Test
+	public void deveRemoverTarefaComSucesso() throws MalformedURLException {
+		WebDriver driver = acessarAplicacao();
+		try {
+			//Inserir tarefa
+			driver.findElement(By.id("addTodo")).click();
+	
+			driver.findElement(By.id("task")).sendKeys("Teste via Selenium");
+			
+			driver.findElement(By.id("dueDate")).sendKeys("10/10/2020");
+			
+			driver.findElement(By.id("saveButton")).click();
+			
+			String message = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Success!", message);
+			
+			//Remover tarefa
+			driver.findElement(By.xpath("//a[@class='btn btn-outline-danger btn-sn']")).click();
+			message = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Success!", message);
+		} finally {
+			
+			// Fechar o browser
+			driver.quit();
+		}
+
+	}
 
 }
